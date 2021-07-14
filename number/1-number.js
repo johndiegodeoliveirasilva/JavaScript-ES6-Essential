5f57286a7493cd929d2ea7dbf3da27395f58ed57
John Diego <johnoliveirasilva9@gmail.com>
2021-07-13 21:52:49 -0300

    number

---
+++ number/1-number.js
@@ -0,0 +1,11 @@
+const mynumber = 12.4032;
+
+// Transformar número para string
+
+const numberAsString = mynumber.toString();
+
+// Retorna número com um número de casas decimais
+const fixedTwoDecimals = mynumber.toFixed(2);
+
+// Transform uma string em float
+const numberAsFloat = parseFloat(mynumber);
