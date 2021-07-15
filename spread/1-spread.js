540df1fb902422ab409bd5963a82d5d9c48db0b2
John Diego <johnoliveirasilva9@gmail.com>
2021-07-14 22:09:34 -0300

    spread

---
+++ spread/1-spread.js
@@ -0,0 +1,11 @@
+// Spread ...
+var partes = ['ombro', 'joelhos'];
+var music = ['cabeca', ...partes, 'e', 'pés'];
+
+console.log(music)
+function fn(x, y, z) {}
+
+var args = [0, 1, 2];
+
+fn(...args);
+
